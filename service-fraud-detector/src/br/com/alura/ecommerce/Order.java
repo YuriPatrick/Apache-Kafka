34c36c3b871592f5e9283f22a68f0032a6cf464e
Yuri Furquim <yuripatrick347@gmail.com>
2020-11-30 12:20:29 -0300

    Fast delegate, evolution and cluster of brokers

--- service-fraud-detector/src/br/com/alura/ecommerce/Order.java
+++ service-fraud-detector/src/br/com/alura/ecommerce/Order.java
@@ -4,12 +4,25 @@ import java.math.BigDecimal;
 
 public class Order {
 
-    private final String userId, orderId;
+    private final String orderId, email;
     private final BigDecimal amount;
 
-    public Order(String userId, String orderId, BigDecimal amount) {
-        this.userId = userId;
+    public Order(String email, String orderId, BigDecimal amount) {
+    	this.email = email;
         this.orderId = orderId;
         this.amount = amount;
     }
+    
+	public String getEmail() {
+		return email;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", email=" + email + ", amount=" + amount + "]";
+	}
 }
--- service-users/src/br/com/alura/ecommerce/Order.java
+++ service-users/src/br/com/alura/ecommerce/Order.java
@@ -4,12 +4,19 @@ import java.math.BigDecimal;
 
 public class Order {
 
-    private final String orderId, email;
+    private final String orderId,email;
     private final BigDecimal amount;
 
-    public Order( String orderId, BigDecimal amount, String email) {
+    public Order(String orderId, BigDecimal amount, String email) {
         this.orderId = orderId;
         this.amount = amount;
         this.email = email;
     }
+    
+   
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return email;
+	}
 }
